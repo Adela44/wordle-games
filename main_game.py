e83adf95f7301d21abda18a4d922ea3d004eb076
Adela <adela-maria.codrean@student.upt.ro>
2025-11-30 20:23:16 +0200

    changed some comments

--- main_game.py
+++ main_game.py
@@ -9,26 +9,26 @@ RESET = "\033[0m"
 
 """
 
-#each line contains one word
+# each line contains one word
 def load_dictionary(file_path):
     with open(file_path) as f:
         words = [line.strip() for line in f]
     return words
 
 def is_valid_guess(guess, guesses):
-    return guess in guesses #we check if the word exists in the guesses list
+    return guess in guesses # check if the word exists in the guesses list
 
-def evaluate_guess(guess, word):  #we return green/yellow or grey letters
+def evaluate_guess(guess, word):  # return green/yellow/grey letters accordingly
     str = ""
     n = len(guess)
     for i in range(n):
         if i < len(word) and guess[i] == word[i]: #if it's in the correct location
             str += "\033[32m" + guess[i]
         else:
-            if guess[i] in word: #if the word contains the correct letter
+            if guess[i] in word: # if the word contains the correct letter
                 str += "\033[33m" + guess[i]
             else:
-                str += "\033[0m" + guess[i] #the normal color, greyish
+                str += "\033[0m" + guess[i] # the normal color, grey-ish
     return str + "\033[0m"
 
 
@@ -38,14 +38,14 @@ def wordle(guesses, answers):
     attempts = 1
     max_attempts = 5
     while attempts <= max_attempts:
-        guess = input("Enter Guess #" + str(attempts) + ": ").lower() #makes the letter lowercase anyways
+        guess = input("Enter Guess #" + str(attempts) + ": ").lower() # makes the letter lowercase (in case of uppercase letters)
         if not is_valid_guess(guess, guesses):
             print("Invalid guess. Please enter a Valid Fruit name.")
             continue
         if guess == secret_word: # the correct guess
             print("You guessed the word! Congratulations! ",secret_word)
             break
-        attempts += 1 #else continue with the attempts
+        attempts += 1 # else continue with the attempts
         feedback = evaluate_guess(guess, secret_word)
         print(feedback)
         if attempts > max_attempts:
